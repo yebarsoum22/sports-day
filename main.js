24d8d7f31095231ce2dc6f733eb6f461fdbfdee2
Youssef Barsoum <yebarsoum22@gmail.com>
2026-08-04 19:09:50 +0000

    duplication refactor

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function notAvailable() {
     alert("Ping pong brackets not available yet!");
 }
+
+document.querySelector('.nav').innerHTML =
+  '<ul class="menu">' +
+  '<li><a href="/index.html">home</a></li>' +
+  '<li><a href="/details.html">event details 2025</a></li>' +
+  '<li><a href="https://docs.google.com/document/d/1PW4E5_o-xSeyjt3Q9emMtEkjtIKmytMeoldiw-Ur7v8/edit?usp=sharing">rules &amp; guidelines</a></li>' +
+  '<li><a href="https://docs.google.com/spreadsheets/d/1XywAxvneE42BS5IP865o05QL2FeEBd_fNuN5_QtL_lA/edit?usp=sharing">complete schedule</a></li>' +
+  '<li><a href="https://faq.canlansports.com/locations/us/il/libertyville/">facility website</a></li>' +
+  '</ul>';
