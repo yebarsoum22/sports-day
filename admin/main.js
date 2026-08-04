a720f599b51b010d20ea4438e66ac209f189243f
Youssef Barsoum <yebarsoum22@gmail.com>
2026-08-04 20:03:14 +0000

    actually remove "complete schedule"

--- admin/main.js
+++ admin/main.js
@@ -7,6 +7,7 @@ document.querySelector('.nav').innerHTML =
   '<li><a href="/index.html">home</a></li>' +
   '<li><a href="/details.html">event details 2026</a></li>' +
   '<li><a href="https://docs.google.com/document/d/1PW4E5_o-xSeyjt3Q9emMtEkjtIKmytMeoldiw-Ur7v8/edit?usp=sharing">rules &amp; guidelines</a></li>' +
-  // '<li><a href="https://docs.google.com/spreadsheets/d/1XywAxvneE42BS5IP865o05QL2FeEBd_fNuN5_QtL_lA/edit?usp=sharing">complete schedule</a></li>' +
   '<li><a href="https://faq.canlansports.com/locations/us/il/libertyville/">facility website</a></li>' +
   '</ul>';
+
+// '<li><a href="https://docs.google.com/spreadsheets/d/1XywAxvneE42BS5IP865o05QL2FeEBd_fNuN5_QtL_lA/edit?usp=sharing">complete schedule</a></li>'
