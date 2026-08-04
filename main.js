5b4522cf0abc63c9dfe0d4c83320c12cd51e6cad
Youssef Barsoum <yebarsoum22@gmail.com>
2026-08-04 20:08:58 +0000

    update navbar

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ document.querySelector('.nav').innerHTML =
   '<ul class="menu">' +
   '<li><a href="/index.html">home</a></li>' +
   '<li><a href="/details.html">event details 2026</a></li>' +
-  '<li><a href="https://docs.google.com/document/d/1PW4E5_o-xSeyjt3Q9emMtEkjtIKmytMeoldiw-Ur7v8/edit?usp=sharing">rules &amp; guidelines</a></li>' +
-  '<li><a href="https://faq.canlansports.com/locations/us/il/libertyville/">facility website</a></li>' +
+  '<li><a href="https://docs.google.com/document/d/1PW4E5_o-xSeyjt3Q9emMtEkjtIKmytMeoldiw-Ur7v8/edit?usp=sharing" target="_blank" rel="noopener">rules &amp; guidelines</a></li>' +
+  '<li><a href="https://faq.canlansports.com/locations/us/il/libertyville/" target="_blank" rel="noopener">facility website</a></li>' +
   '</ul>';
 
 // '<li><a href="https://docs.google.com/spreadsheets/d/1XywAxvneE42BS5IP865o05QL2FeEBd_fNuN5_QtL_lA/edit?usp=sharing">complete schedule</a></li>'
